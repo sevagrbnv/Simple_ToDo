fdc64abee7fc80b3f2d3590c99fe84e629bcc71a
seva_ <sevagrbnv@gmail.com>
2022-12-25 22:49:45 +0300

    rollback

--- app/src/main/java/com/example/simpletodo/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/simpletodo/presentation/MainViewModel.kt
@@ -1,23 +1,35 @@
 package com.example.simpletodo.presentation
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.simpletodo.data.TodoListRepositoryImpl
+import com.example.simpletodo.domain.DeleteTodoItemUseCase
+import com.example.simpletodo.domain.EditTodoItemUseCase
+import com.example.simpletodo.domain.GetTodoListUseCase
+import com.example.simpletodo.domain.TodoItem
 
 class MainViewModel : ViewModel() {
 
-    private val repository = com.example.simpletodo.data.TodoListRepositoryImpl
+    private val repository = TodoListRepositoryImpl
 
-    private val getTodoListUseCase = com.example.simpletodo.domain.GetTodoListUseCase(repository)
-    private val deleteTodoListUseCase =
-        com.example.simpletodo.domain.DeleteTodoItemUseCase(repository)
-    private val editTodoItemUseCase = com.example.simpletodo.domain.EditTodoItemUseCase(repository)
+    private val getTodoListUseCase = GetTodoListUseCase(repository)
+    private val deleteTodoListUseCase = DeleteTodoItemUseCase(repository)
+    private val editTodoItemUseCase = EditTodoItemUseCase(repository)
 
     val todoList = getTodoListUseCase.execute()
 
-    fun deleteTodoItem(todoItem: com.example.simpletodo.domain.TodoItem) {
+//    private val _isEmptyList = MutableLiveData<Boolean>()
+//    val isEmptyList: LiveData<Boolean>
+//        get() = _isEmptyList
+
+    fun deleteTodoItem(todoItem: TodoItem) {
         deleteTodoListUseCase.execute(todoItem)
     }
 
     // for change state of checkbox on item
-    fun changeItemCompleteState(todoItem: com.example.simpletodo.domain.TodoItem) {
+    fun changeItemCompleteState(todoItem: TodoItem) {
         val newItem = todoItem.copy(isComplete = !todoItem.isComplete)
         editTodoItemUseCase.execute(newItem)
     }
